1b6c5268db32a00572d6817d506619d12a48b478
crystalneth <konsti7@gmx.net>
2017-07-02 18:58:25 +0200

    added contact form and button linkings

--- app/components/landing/landingController.js
+++ app/components/landing/landingController.js
@@ -8,13 +8,32 @@ angular.module('studyscriptApp')
 // Define controllers
   .controller('landingController', LandingController);
 
-LandingController.$inject = ['$location'];
+LandingController.$inject = ['$scope'];
 
 /* @ngInject */
-function LandingController($location) {
+function LandingController($scope) {
   let self = this;
 
-  // self.order = function () {
-  //   $location.path('/order');
-  // };
+  self.disable = false;
+  self.error = false;
+
+  self.saveData = function () {
+    self.disable = false;
+    if (self.email) {
+      firebase.database().ref('prospects').push(self.email)
+        .then(function () {
+          self.disable = true;
+          self.error = false;
+          self.email = '';
+          $scope.$apply();
+        })
+        .catch(function (err) {
+          self.disable = false;
+          self.error = true;
+          console.log(err.message);
+        });
+    } else {
+      self.error = true;
+    }
+  };
 }
